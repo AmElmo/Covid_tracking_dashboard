e2b95ab4a341a161e1cfd7068c6b637ea4fd8f1c
Julien Berthomier <berthomier.julien@gmail.com>
2023-02-06 19:00:33 +0000

    packages definition

--- packages/vaccination.py
+++ packages/vaccination.py
@@ -7,3 +7,26 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+## Dictionaries loading
+
+# Dictionary of iso codes / population (> 300.000)
+def dict_population_iso(data):
+
+    country_codes = data['iso_code'].unique()
+    dict_population = {}
+
+    for code in country_codes:
+        response = requests.get("https://restcountries.com/v2/alpha/", params={'codes':code})
+
+        # Ignore 404 responses and only include countries with >= 300000 inhabitants
+        if response.status_code == 200 and response.json()[0]['population'] >= 300000:
+            population = response.json()[0]['population']
+            dict_population[code] = population
+            print(code)
+        else:
+            continue
+
+    return dict_population
+
+## Country level functions
