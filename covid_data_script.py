3e413aa0def855b43d0ec6725545f39ca954e1b2
Julien Berthomier <berthomier.julien@gmail.com>
2023-02-10 19:00:33 +0000

    package definition

--- covid_data_script.py
+++ covid_data_script.py
@@ -21,8 +21,29 @@ cd_data = cd.csv_to_dataframe("cases_deaths_data.csv")
 
 vc_data = cd.csv_to_dataframe("vaccination_data.csv")
 
-# Load dictionaries
+# Load dictionaries for cases & deaths
+## Generate dictionary with country code + population
+dict_code_pop = cd.dict_population(cd_data)
 
+# Generate dictionary with country code + country name
+def dictionary_country_code(data):
+
+    print("Start dictionary")
+
+    list_codes = data['Country_code'].unique()
+    list_countries = data['Country'].unique()
+
+    dict_codes = {'Country_code': list_codes, 'Country': list_countries}
+
+    df = pd.DataFrame(dict_codes)
+
+    final_dict = dict(zip(df['Country_code'], df["Country"]))
+
+    print("Country code / country name dictionary created")
+
+    return final_dict
+
+# Generate dictionary of country names + WHO_region
 
 
 # Run manipulation scripts & push to Google Sheet API
