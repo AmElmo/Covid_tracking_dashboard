f117abdc78ba694213caab274fdbd242a8165e81
Julien Berthomier <berthomier.julien@gmail.com>
2023-02-17 19:00:33 +0000

    package definition

--- covid_packages/googlesheet_api.py
+++ covid_packages/googlesheet_api.py
@@ -8,7 +8,30 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-def push_sheet_api(SCOPES,SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME):
+def clean_sheet_api(SCOPES,SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME):
+    if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret_59863169565-0g6psho3kj61kokqdsrqiao73m0o1i4q.apps.googleusercontent.com.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet = service.spreadsheets()
+
+    request = sheet.values().clear(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Sheet1!1:1000").execute()
+
+
+
+def push_sheet_api(SCOPES,SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME,VALUES):
 
     if os.path.exists('token.json'):
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -34,14 +57,11 @@ def push_sheet_api(SCOPES,SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME):
 
     value_range_body = {
         "majorDimension": "ROWS",
-        "values": [["valuea1"], ["valuea2"], ["valuea3"]]
+        "values": VALUES
     }
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-
-    request = sheet.values().clear(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Sheet1!1:1000").execute()
-
 
     result = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                     range=SAMPLE_RANGE_NAME, valueInputOption="USER_ENTERED", body=value_range_body).execute()
