6ff26374b2cf1b5a618704ddf7a0a05fdf872a4d
Julien Berthomier <berthomier.julien@gmail.com>
2023-02-23 19:00:33 +0000

    package definition

--- covid_data_script.py
+++ covid_data_script.py
@@ -7,8 +7,8 @@ import requests
 # Variables for Google Sheet API
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SAMPLE_SPREADSHEET_ID = '1ESRCmW23Rb69f5mIfBhV_yy1FsC92f4W8AmxglWnClU'
-SAMPLE_RANGE_NAME = 'Sheet1!A2:E'
+SPREADSHEET_ID = '1ESRCmW23Rb69f5mIfBhV_yy1FsC92f4W8AmxglWnClU'
+RANGE_NAME = 'Sheet1!A2:E'
 
 # Download the 2 files from WHO website
 
@@ -51,18 +51,19 @@ dict_iso_country =vc.dictionary_iso_code(vc_data)
 ## Cases & Deaths
 
 ### Number of cases (last day)
+sheetName = "Number of cases (last day)"
 
-gs.renameSheet(SCOPES,SAMPLE_SPREADSHEET_ID,"Number of cases (last day)")
+gs.createSheet(SCOPES,SPREADSHEET_ID,sheetName)
 
 data = []
 
-gs.clean_sheet_api(SCOPES,SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+gs.clean_sheet_api(SCOPES,SPREADSHEET_ID,RANGE_NAME)
 
 for code, name in dict_code_name.items():
 
     data.append([name,str(cd.new_cases_lastday(cd_data,name))])
 
-gs.push_sheet_api(SCOPES,SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME,data)
+gs.push_sheet_api(SCOPES,SPREADSHEET_ID,RANGE_NAME,data)
 
 ### Number of deaths (last day)
 
