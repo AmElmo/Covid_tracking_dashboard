f5198f1cfcd625559c51a3a6b0d3729589e6165e
Julien Berthomier <berthomier.julien@gmail.com>
2023-07-18 17:49:54 +0100

    Main script

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,38 @@
 import dash
 import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd
 from google.cloud import bigquery
 
 client = bigquery.Client()
+
+# Query for the data
+
+query = """
+SELECT *
+FROM `my_project.my_dataset.my_table`
+"""
+df_1 = client.query(query).to_dataframe()
+
+
+# Build the layout for the Dash app
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df['column1'], 'y': df['column2'], 'type': 'bar', 'name': 'Data'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
