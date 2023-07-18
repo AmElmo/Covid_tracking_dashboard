8e0d26cef2a9c0352d7acd65370fca4886fa5c6c
Julien Berthomier <berthomier.julien@gmail.com>
2023-07-18 18:18:59 +0100

    Main script

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,23 @@
 import dash
-import dash_core_components as dcc
+from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd
 from google.cloud import bigquery
+import os
 
-client = bigquery.Client()
+client = bigquery.Client(location="US", project="covid-dashboard-378011")
+
+bigquery_ref = 'covid-dashboard-378011.covid_data_script'
+
+key_path = "covid-dashboard-378011-d39bea98e1ae.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
 # Query for the data
 
-query = """
+query = f"""
 SELECT *
-FROM `my_project.my_dataset.my_table`
+FROM {bigquery_ref}.`cum_caseslatest`
 """
 df_1 = client.query(query).to_dataframe()
 
@@ -25,7 +31,7 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': df['column1'], 'y': df['column2'], 'type': 'bar', 'name': 'Data'},
+                {'x': df_1['Country'], 'y': df_1['Cum_caseslatest'], 'type': 'bar', 'name': 'Data'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
